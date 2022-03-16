b4a0467c949263c8863ebdfc417a8965ef9d8625
Eva G Cagigas <evarteca@gmail.com>
2022-03-16 20:26:54 +0100

    Navbar & Logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import logoImg from "../assets/logo-balloon-to-mars.jpg"
+import { NavLink } from "react-router-dom"
+
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div>
+
+      <img src={logoImg} alt="Logo" width="350rem" />
+
+      <NavLink to="/"> <button>Home</button> </NavLink>
+
+      <NavLink to="/weather"> <button>Weather in Mars</button> </NavLink>
+
+      <NavLink to="/image-gallery"> <button>Gallery</button> </NavLink>
+
+      <NavLink to="/another-places"> <button>Another Places</button> </NavLink>
+
+      <NavLink to="/astronomical-events"> <button>Astronomic Events</button> </NavLink>
+
+    </div>
   )
 }
 
