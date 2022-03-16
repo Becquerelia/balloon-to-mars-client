c1c425ee3219f29b3e1e99d9693a6295c3bcfa54
Eva G Cagigas <evarteca@gmail.com>
2022-03-16 23:37:35 +0100

    navbar toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,20 +4,66 @@ import { NavLink } from "react-router-dom"
 
 function Navbar() {
   return (
-    <div>
-
-      <img src={logoImg} alt="Logo" width="350rem" />
-
-      <NavLink to="/"> <button>Home</button> </NavLink>
-
-      <NavLink to="/weather"> <button>Weather in Mars</button> </NavLink>
-
-      <NavLink to="/image-gallery"> <button>Gallery</button> </NavLink>
-
-      <NavLink to="/another-places"> <button>Another Places</button> </NavLink>
-
-      <NavLink to="/astronomical-events"> <button>Astronomic Events</button> </NavLink>
-
+    <div className="navbar navbar-expand-lg navbar-dark">
+     <div className="container-fluid">       
+       <img src={logoImg} alt="Logo" width="350rem" className="navbar-brand" />
+       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+         <span className="navbar-toggler-icon"></span>
+        </button> 
+       <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <NavLink to="/"> 
+                {({isActive})=> {
+                  return <button id="btn-home" className={ isActive ? "nav-active" : "nav-unactive" }> Home </button>
+                }} 
+              </NavLink>
+            </li> 
+            <li className="nav-item">
+              <NavLink to="/weather"> 
+                {({isActive})=> {
+                  return <button className={ isActive ? "nav-active" : "nav-unactive" }> Weather in Mars </button>
+                }} 
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/image-gallery"> 
+                {({isActive})=> {
+                  return <button className={ isActive ? "nav-active" : "nav-unactive" }> Gallery </button>
+                }} 
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/another-places"> 
+                {({isActive})=> {
+                  return <button className={ isActive ? "nav-active" : "nav-unactive" }> Another Places </button>
+                }} 
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink to="/astronomical-events"> 
+                {({isActive})=> {
+                  return <button className={ isActive ? "nav-active" : "nav-unactive" }> Astronomic Events </button>
+                }} 
+              </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink to="/login"> 
+                {({isActive})=> {
+                  return <button className={ isActive ? "nav-active" : "nav-unactive" }> Log In </button>
+                }} 
+              </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink to="/signup"> 
+                {({isActive})=> {
+                  return <button id="btn-signup" className={ isActive ? "nav-active" : "nav-unactive" }> Sign Up </button>
+                }} 
+              </NavLink>
+            </li>        
+          </ul>           
+        </div>        
+      </div> 
     </div>
   )
 }
