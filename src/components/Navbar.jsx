1d4502da3801e4e6d90cef52504e1573a373987c
Eva G Cagigas <evarteca@gmail.com>
2022-03-18 12:10:14 +0100

    navbar design fixed & gallery page in progress

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,51 +5,57 @@ import { NavLink } from "react-router-dom"
 //!MAIN FUNCTION:
 function Navbar() {
   return (
-    <div className="navbar navbar-expand-lg navbar-dark">
+    <div className="navbar">
+      <div>
+        <img src={logoImg} alt="Logo" width="350rem" />
+      </div>
+      
+      <div className="aline-navbar" >      
+        <NavLink to="/"> 
+          {({isActive})=> {
+            return <button id="btn-left" className={ isActive ? "nav-active" : "nav-unactive" }> Home </button>
+          }} 
+        </NavLink>
 
-      <img src={logoImg} alt="Logo" width="350rem" />
+        <NavLink to="/weather"> 
+          {({isActive})=> {
+            return <button className={ isActive ? "nav-active" : "nav-unactive" }> Weather in Mars </button>
+          }} 
+        </NavLink>
 
-      <NavLink to="/"> 
-        {({isActive})=> {
-          return <button id="btn-left" className={ isActive ? "nav-active" : "nav-unactive" }> Home </button>
-        }} 
-      </NavLink>
+        <NavLink to="/image-gallery"> 
+          {({isActive})=> {
+             return <button className={ isActive ? "nav-active" : "nav-unactive" }> Gallery </button>
+          }} 
+        </NavLink>
 
-      <NavLink to="/weather"> 
-        {({isActive})=> {
-          return <button className={ isActive ? "nav-active" : "nav-unactive" }> Weather in Mars </button>
-        }} 
-      </NavLink>
+        <NavLink to="/another-places"> 
+          {({isActive})=> {
+            return <button className={ isActive ? "nav-active" : "nav-unactive" }> Another Places </button>
+          }} 
+        </NavLink>
 
-      <NavLink to="/image-gallery"> 
-        {({isActive})=> {
-           return <button className={ isActive ? "nav-active" : "nav-unactive" }> Gallery </button>
-        }} 
-      </NavLink>
+        <NavLink to="/astronomical-events"> 
+          {({isActive})=> {
+            return <button id="btn-right" className={ isActive ? "nav-active" : "nav-unactive" }> Astronomical Events </button>
+          }} 
+        </NavLink>
 
-      <NavLink to="/another-places"> 
-        {({isActive})=> {
-          return <button className={ isActive ? "nav-active" : "nav-unactive" }> Another Places </button>
-        }} 
-      </NavLink>
+      </div>
+        
+      <div>
+        <NavLink to="/login"> 
+          {({isActive})=> {
+            return <button id="btn-left" className={ isActive ? "nav-active" : "nav-unactive" }> Log In </button>
+          }} 
+        </NavLink>
 
-      <NavLink to="/astronomical-events"> 
-        {({isActive})=> {
-          return <button id="btn-right" className={ isActive ? "nav-active" : "nav-unactive" }> Astronomical Events </button>
-        }} 
-      </NavLink>
-
-      <NavLink to="/login"> 
-        {({isActive})=> {
-          return <button id="btn-left" className={ isActive ? "nav-active" : "nav-unactive" }> Log In </button>
-        }} 
-      </NavLink>
-
-      <NavLink to="/signup"> 
-        {({isActive})=> {
-          return <button id="btn-right" className={ isActive ? "nav-active" : "nav-unactive" }> Sign Up </button>
-        }} 
-      </NavLink>
+        <NavLink to="/signup"> 
+          {({isActive})=> {
+            return <button id="btn-right" className={ isActive ? "nav-active" : "nav-unactive" }> Sign Up </button>
+          }} 
+        </NavLink>
+      </div>
       
     </div>
   )
--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,6 +1,43 @@
+//!IMPORTS:
 import galleryLettersImg from "../assets/gallery-letters.png"
+import {useState, useEffect} from 'react'
+import {Link, useNavigate} from "react-router-dom"
+import RingLoader from "react-spinners/RingLoader";
+import axios from "axios";
 
+//!MAIN FUNCTION:
 function Gallery() {
+
+  //!CONSTANTS & HOOKS:
+  const [allImages, setAllImages] = useState(null)
+  const navigate = useNavigate()
+  
+  useEffect(()=>{
+    getAllImages()
+  }, [])
+
+  //!INTERNAL FUNCTIONS:
+  const getAllImages = async () => {
+    try {
+      const response = await axios.get("http://localhost:5005/api/image-gallery")
+      console.log(response.data)      
+    }
+    catch(err){
+      navigate("/error")
+    }
+  }
+
+  //!LOADING SYSTEM:
+  if(!allImages){ 
+    return (
+      <div>
+        <RingLoader color="#C83B30" size="10rem" />
+        <h2>Loading...</h2>
+      </div>
+    )
+  }
+
+  //!RENDER VIEW:
   return (
     <div>
       <img src={galleryLettersImg} alt="Logo" width="700rem"/>
